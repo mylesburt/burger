0f2b3777cefabdff3d95db222acabca6f048fb6e
Myles Burt <myles_burt@rocketmail.com>
2020-11-22 20:13:58 +1030

    JAWS DB add-on code added to connection.js for Heroku deployment.

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+connection = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASS,
   database: process.env.MYSQL_DATABASE
 });
+};
+
+
 
 // Make connection.
 connection.connect(function(err) {
