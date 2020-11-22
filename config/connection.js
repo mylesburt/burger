2366917205075d1692cd9463825049ca23491dfe
Myles Burt <myles_burt@rocketmail.com>
2020-11-22 14:57:37 +1030

    Dotenv npm installed, env file created with keys for connection.js, env example created to help the user operate the app.

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "#codingismypassion20",
-  database: "burger_db"
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASS,
+  database: process.env.MYSQL_DATABASE
 });
 
 // Make connection.
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var dotenv = require('dotenv').config()
 
 var PORT = process.env.PORT || 8080;
 
